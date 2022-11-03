268f711360b17c97bb4d2a0b5e6b9f7899868794
202122430327 郭勤 <123456789>
2022-11-03 21:27:07 +0800

    添加了fun()方法

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+       fun();
+    }
+    public static void fun(){
+        System.out.println("你好");
     }
 }
